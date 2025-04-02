3123d32de4115fbe3ade7a36ed896dc41332a605
SupriyoDas10 <supriyodas44332@gmail.com>
2025-04-02 22:48:49 +0530

    Initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { assets } from '../../assets/assets';
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer'>
+        <div className="footer" id="footer">
+            <div className="footer-content">
+                <div className="footer-content-left">
+         <p alt="" className="log1" > KhanaPina </p>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti dicta eum numquam? Maxime beatae numquam ab ipsam, cumque natus nulla corporis fugiat, animi, aut cum! Dolores libero odit culpa eligendi!</p>
+             <div className="footer-social-icon">
+                <img src={assets.facebook_icon} alt="" />
+                <img src={assets.twitter_icon} alt="" />
+                <img src={assets.linkedin_icon} alt="" />
+                </div>  
+               
+                </div>
+                <div className="footer-content-right">
+               <h2>COMPANY</h2>
+               <ul>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Delivery</li>
+                <li>Privacy policy</li>
+               </ul>
+                </div>
+                <div className="footer-content-center">
+<h2>GET IN TOUCH</h2>
+<ul>
+    <li>9999-922-222</li>
+    <li>contact@KhanaPina.com</li>
+</ul>
+                </div>
+            </div>
+            <hr />
+            <p className="footer-copyright">Copyright 2024 © KhanaPina.com-All Right Reserved </p>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
