75af6dad967c3cf0877a316fb720e3336330c471
Supriyo Das <142605328+supriyodas10@users.noreply.github.com>
2025-04-03 13:25:48 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -35,7 +35,7 @@ const Footer = () => {
                 </div>
             </div>
             <hr />
-            <p className="footer-copyright">Copyright 2024 © KhanaPina.com-All Right Reserved </p>
+            <p className="footer-copyright">Copyright 2024 © KhanaPina.com-All Right Reserved by Supriyo </p>
         </div>
     </div>
   )
